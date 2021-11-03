dd8691f033e600bfea68851578e38f45ff90e8e0
Nadine Han <nadine@dhcp-10-250-6-55.harvard.edu>
2021-11-02 23:34:26 -0400

    Part 4 Redo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import { BrowserRouter } from 'react-router-dom';
+
+import { Provider } from 'react-redux';
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/auth';
+import 'firebase/functions';
+import { createStore, combineReducers } from 'redux';
+import {
+  ReactReduxFirebaseProvider,
+  firebaseReducer,
+} from 'react-redux-firebase';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAC0qaud5YwkNZV7ADHcwf9rpoGH5TPqEs',
+  authDomain: 'bootcamp-ce748.firebaseapp.com',
+  databaseURL: 'https://bootcamp-ce748.firebaseio.com',
+  projectId: 'bootcamp-ce748',
+  storageBucket: 'bootcamp-ce748.appspot.com',
+  messagingSenderId: '782124421793',
+  appId: '1:782124421793:web:b26b23317989a449dad6e0',
+};
+
+firebase.initializeApp(firebaseConfig);
+// firebase.functions().useFunctionsEmulator('http://localhost:5001');
+
+// Add firebase to reducers
+const rootReducer = combineReducers({
+  firebase: firebaseReducer,
+});
+
+// Create store with reducers and initial state
+const store = createStore(rootReducer, composeWithDevTools());
+
+// react-redux-firebase config
+const rrfConfig = {
+  preserveOnLogout: ['homepage'],
+  userProfile: 'users',
+};
+
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ReactReduxFirebaseProvider>
+  </Provider>,
+  document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
