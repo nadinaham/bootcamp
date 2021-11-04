e1aafb7d0fe6805899f480a269f2e288b9b81744
Nadine Han <nadine@dhcp-10-250-6-55.harvard.edu>
2021-11-03 20:57:33 -0400

    Part 4 Final

--- src/CardViewer.js
+++ src/CardViewer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './CardViewer.css';
 
 class CardViewer extends React.Component {
     constructor(props){
@@ -8,42 +9,8 @@ class CardViewer extends React.Component {
             index:0,
             display:""
         }
-        console.log("inside viewer 1");
-        this.state.display = ""
-        console.log("inside viewer 2");
     }
-  render() {
-    this.props.cards.length !== 0 ? this.state.display=this.props.cards[this.state.index].front : "";
-    return (
-        <div className={"container"}>
-						<h1>Flashcards!!</h1>
-                        Card {this.state.index + 1} out of {this.props.cards.length}
-						<div>
-						<table>
-							<tbody>
-								<tr>
-									<td colspan="3">
-										<h1>
-											{this.state.display === "" ? "" : this.state.front ? this.props.cards[this.state.index].front : this.props.cards[this.state.index].back}	
-										</h1>
-									</td>
-								</tr>
-								<tr>
-									<td><button onClick={this.lookAnswer}>{ this.state.display === "" ? "Add Card in Card Editor" : this.state.front ? "Answer" : "Question" }</button></td>
-									<td><button onClick={this.incrementIndex}>Next Card</button></td>
-                                    <td><button onClick={this.subtractIndex}>Previous Card</button></td>
-								</tr>
-							</tbody>
-						</table>  
-						</div>	
-        <h2>console.log("Card"+progress+"out of"+)
-        <h2>Card Viewer</h2>
-        <hr />
-        <button onClick={this.props.switchMode}>Go to card editor</button>
-      </div>
-    );
-  }
-  
+
 lookAnswer = () => {
     this.setState(state => ({
         front: !state.front,
@@ -65,6 +32,28 @@ subtractIndex = () => {
         front: true
     }));
 }
+
+  render() {
+      const card = this.props.cards[this.state.index][this.state.front ? 'front' : 'back'];    
+      return (
+        <div>
+        Card {this.state.index + 1} out of {this.props.cards.length}
+        <div className="card" onClick={this.lookAnswer}>
+            {card}
+        </div>
+        
+        <br />
+        
+        <button onClick={this.subtractIndex}>Previous card</button>
+        <button onClick={this.incrementIndex}>Next card</button>	
+
+        <h2>Card Viewer</h2>
+        <hr />
+        <button onClick={this.props.switchMode}>Go to card editor</button>
+      </div>
+    );
+  }
+
 
 }
 
